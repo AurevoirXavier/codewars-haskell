d14b9233763218e0292917a462a980f13354d428
AurevoirXavier <c.estlavie@icloud.com>
2018-01-03 19:58:30 +0800

    19:58/01/03/2018

--- 8kyu/Is the string uppercase?/lib.hs
+++ 8kyu/Is the string uppercase?/lib.hs
@@ -2,8 +2,6 @@ module Codewars.Kata.IsUpperCase where
     
 import Data.Char (isLower)
     
--- isUpperCase :: String -> Bool
--- isUpperCase = all isUpper . filter isAlpha
-
 isUpperCase :: String -> Bool
+-- isUpperCase = all isUpper . filter isAlpha
 isUpperCase = all (not . isLower)
