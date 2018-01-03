2d35c1e14ad8bea3ffe2a20af785d91305a66a3f
AurevoirXavier <c.estlavie@icloud.com>
2018-01-03 19:10:42 +0800

    19:10/01/03/2018

--- 8kyu/Bin to Decimal/lib.hs
+++ 8kyu/Bin to Decimal/lib.hs
@@ -1,4 +1,10 @@
 module BinToDecimal where
 
+-- binToDec :: String -> Int
+-- binToDec = fst . foldr (\c (n, i) -> if c == '1' then (n + 2 ^ i, i + 1) else (n, i + 1)) (0, 0)
+
 binToDec :: String -> Int
-binToDec s = 0
+binToDec = foldl1 ((+) . (2 *)) . map f
+  where
+    f '0' = 0
+    f _ = 1
