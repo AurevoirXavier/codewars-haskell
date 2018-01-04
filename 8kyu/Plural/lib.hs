1fb84748cb081822960b865bbeb2607ecaf1a003
AurevoirXavier <c.estlavie@icloud.com>
2018-01-04 23:20:38 +0800

    23:20/01/04/2018

--- 8kyu/Plural/lib.hs
+++ 8kyu/Plural/lib.hs
@@ -1,7 +1,10 @@
 module Codewars.Kata.Plural where
 
 import Codewars.Kata.Plural.Types
-    -- data Grammar = Singular | Plural
+
+data Grammar
+  = Singular
+  | Plural
 
 plural :: (Num a, Eq a) => a -> Grammar
 plural n = error "todo: plural"
