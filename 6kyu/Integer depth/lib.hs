94c1ddb47dc0c88eb5c451f5e71f12691109950a
AurevoirXavier <c.estlavie@icloud.com>
2018-01-10 22:12:44 +0800

    22:12/01/10/2018

---
+++ 6kyu/Integer depth/lib.hs
@@ -0,0 +1,11 @@
+module ComputeDepth where
+
+import           Data.Set (empty, fromList, union)
+computeDepth :: Int -> Int
+computeDepth n = go empty 0
+    where
+        go acc t
+            | length acc == 10 = t
+            | otherwise = go (acc `union` (fromList . show $ n * t')) t'
+            where
+                t' = t + 1
