6cba73f7841cac772b513b200d59f593519b585e
AurevoirXavier <c.estlavie@icloud.com>
2018-01-10 22:21:35 +0800

    22:21/01/10/2018

--- 6kyu/Integer depth/lib.hs
+++ 6kyu/Integer depth/lib.hs
@@ -6,6 +6,6 @@ computeDepth n = go empty 0
     where
         go acc t
             | length acc == 10 = t
-            | otherwise = go (acc `union` (fromList . show $ n * t')) t'
+            | otherwise        = go (acc `union` (fromList . show $ n * t')) t'
             where
                 t' = t + 1
