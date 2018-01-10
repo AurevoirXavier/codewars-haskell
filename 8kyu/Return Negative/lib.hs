d6137aa3a67668436e92c5aba3b6d691cfcd4d7b
AurevoirXavier <c.estlavie@icloud.com>
2018-01-10 14:29:17 +0800

    14:29/01/10/2018

--- 8kyu/Return Negative/lib.hs
+++ 8kyu/Return Negative/lib.hs
@@ -1,4 +1,7 @@
 module Codewars.Kata.Negative where
 
 makeNegative :: (Num a, Ord a) => a -> a
-makeNegative = error "todo: makeNegative"
+makeNegative n =
+    if n > 0
+        then (-n)
+        else n
