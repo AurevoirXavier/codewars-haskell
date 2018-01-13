f5f70bebd46fc8f9ef92e7112d9867a40a7005e8
AurevoirXavier <c.estlavie@icloud.com>
2018-01-13 14:15:17 +0800

    14:15/01/13/2018

--- 7kyu/Sorted Union/lib.hs
+++ 7kyu/Sorted Union/lib.hs
@@ -1,6 +1,10 @@
 module Codewars.Kata.SortedUnion where
 
 import           Data.List (nub)
+-- import           Data.List (union)
+
 
 uniteUnique :: Ord a => [[a]] -> [a]
 uniteUnique = nub . concat
+
+-- uniteUnique = foldl union []
