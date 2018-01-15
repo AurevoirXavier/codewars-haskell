995207c6a685663717b30917b4b86f6ca8b4245f
AurevoirXavier <c.estlavie@icloud.com>
2018-01-15 21:13:56 +0800

    21:13/01/15/2018

--- 7kyu/Speed Control/lib.hs
+++ 7kyu/Speed Control/lib.hs
@@ -1,4 +1,7 @@
 module Codewars.G964.Gps1 where
 
 gps :: Int -> [Double] -> Int
-gps s x = 
+gps s x =
+    if length x > 1
+        then (`div` s) . floor . (*3600) . maximum $ zipWith (-) (tail x) x
+        else 0
