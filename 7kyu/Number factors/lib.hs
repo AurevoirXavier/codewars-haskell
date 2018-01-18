9e2d8fb071c7c80fb6f870ae5bb56b4fffb54a35
AurevoirXavier <c.estlavie@icloud.com>
2018-01-19 01:06:56 +0800

    01:06/01/19/2018

--- 7kyu/Number factors/lib.hs
+++ 7kyu/Number factors/lib.hs
@@ -1,4 +1,14 @@
 module NumberFactors where
 
+import           Data.List (nub, sort)
+
 getFactors :: Int -> [Int]
-getFactors n = []
+-- getFactors n = nub . sort $ f 1 []
+--     where
+--         root = floor . sqrt $ fromIntegral n
+--         f fac facs
+--             | fac > root = facs
+--             | otherwise   = case n `mod` fac of 0 -> f (fac + 1) (facs ++ [fac, n `div` fac])
+--                                                 _ -> f (fac + 1) facs
+
+getFactors n = [x | x <- [1..n], mod n x == 0]
