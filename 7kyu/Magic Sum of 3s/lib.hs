d3a8344f8d1a08c3e80cbdd6f3288df13899e68e
AurevoirXavier <c.estlavie@icloud.com>
2018-01-23 02:10:39 +0800

    02:10/01/23/2018

--- 7kyu/Magic Sum of 3s/lib.hs
+++ 7kyu/Magic Sum of 3s/lib.hs
@@ -5,3 +5,6 @@ import           Control.Applicative (liftA2)
 magicSum :: [Int] -> Int
 magicSum = sum . filter (liftA2 (&&) odd (elem '3' . show))
 
+-- magicSum = sum . filter ((&&) <$> odd <*> (elem '3' . show))
+
+-- magicSum = sum . filter (\x -> odd x && '3' `elem` show x)
