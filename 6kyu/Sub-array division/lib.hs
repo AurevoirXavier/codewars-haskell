8c476b872fa3265b1db78e2b15b32188455f53eb
AurevoirXavier <c.estlavie@icloud.com>
2018-01-27 23:35:10 +0800

    23:35/01/27/2018

--- 6kyu/Sub-array division/lib.hs
+++ 6kyu/Sub-array division/lib.hs
@@ -1,3 +1,10 @@
 module SubArrayDiv where
 
+import           Data.List (subsequences)
+
 solve :: [Int] -> Int -> Bool
+solve xs n = if any (> n) xs then f else g
+    where
+        xs' = map sum . tail . subsequences $ xs
+        f   = any ((== 0) . (`mod` n)) xs'
+        g   = n `elem` xs'
