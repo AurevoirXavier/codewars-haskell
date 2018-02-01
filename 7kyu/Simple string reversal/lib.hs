a98bd52620b3810264cf1347d5cb81bb57472793
AurevoirXavier <c.estlavie@icloud.com>
2018-02-01 21:49:35 +0800

    21:49/02/01/2018

--- 7kyu/Simple string reversal/lib.hs
+++ 7kyu/Simple string reversal/lib.hs
@@ -1,7 +1,3 @@
 module SimpleReversal where
 
 solve :: String -> String
-solve = go []
-    where
-        go acc []     = acc
-        go acc (x:xs) = if x /= ' ' then go (x:acc) xs else go (' ':acc) xs
