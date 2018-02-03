40c3937d625b552f35f548e294a85a534d585e3e
AurevoirXavier <c.estlavie@icloud.com>
2018-02-03 21:21:17 +0800

    21:21/02/03/2018

--- 7kyu/Simple string reversal/lib.hs
+++ 7kyu/Simple string reversal/lib.hs
@@ -1,7 +1,11 @@
 module SimpleReversal where
 
-    solve :: String -> String
-    solve = unwords . (rev <$> reverse . filter (/= ' ') <*> map length . words)
-        where
-            rev _ []          = []
-            rev remain (x:xs) = let (l, r) = splitAt x remain in l : rev r xs
+import           Data.List.Split (splitPlaces)
+
+solve :: String -> String
+-- solve = unwords . (rev <$> reverse . filter (/= ' ') <*> map length . words)
+--     where
+--         rev _ []          = []
+--         rev remain (x:xs) = let (l, r) = splitAt x remain in l : rev r xs
+
+solve = unwords . (splitPlaces . map length <*> reverse . concat) . words
