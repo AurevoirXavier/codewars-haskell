176778f689d92b0bb2cea38e8b015f0c038fc985
AurevoirXavier <c.estlavie@icloud.com>
2018-02-05 20:11:21 +0800

    20:11/02/05/2018

--- 6kyu/Sum of integer combinations/lib.hs
+++ 6kyu/Sum of integer combinations/lib.hs
@@ -1,20 +1,5 @@
 module SumIntegerCombinations where
 
 find :: [Int] -> Int -> [[Int]]
-find = go []
-    where
-        go combinations xs n = foldl f combinations xs
-            where
-                f acc x
-                    | diff > 0 && x >= diff = [x, diff]:acc
-                    | diff == 0             = [x]:acc
-                    | otherwise             = acc
-                    where
-                        diff = n - x
 
 -- [12, 9, 6, 3]
--- 12
--- 9 3
--- 6 6
--- 3 3 6
--- 3 3 3 3
