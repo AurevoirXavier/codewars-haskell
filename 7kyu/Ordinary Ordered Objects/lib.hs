e6ac12cc06118683674deedb63d66417c34b2592
AurevoirXavier <c.estlavie@icloud.com>
2018-02-07 15:01:09 +0800

    15:01/02/07/2018

--- 7kyu/Ordinary Ordered Objects/lib.hs
+++ 7kyu/Ordinary Ordered Objects/lib.hs
@@ -1,4 +1,7 @@
 module FrequencyFrenzy where
 
+import           Data.List (sort)
+import           Data.Map  (empty, insertWith, toList)
+
 frequency :: Ord a => [a] -> [(a, Int)]
-frequency xs = undefined
+frequency = sort . toList . foldl (\m x -> insertWith (+) x 1 m) empty
