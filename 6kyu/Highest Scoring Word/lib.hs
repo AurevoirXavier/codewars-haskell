e42486943cac16a1723987e3cc8321298275282a
AurevoirXavier <c.estlavie@icloud.com>
2018-02-13 20:54:51 +0800

    20:54/02/13/2018

--- 6kyu/Highest Scoring Word/lib.hs
+++ 6kyu/Highest Scoring Word/lib.hs
@@ -1,5 +1,17 @@
 module High.JorgeVS.Kata where
 
-import           Data.Char ()
+import           Data.Char (ord)
+-- import           Data.Maybe (fromJust)
+
 high :: String -> String
-high myStr =
+high = fst . foldl f ("", 0) . words
+    -- where
+    --     table       = zip ['a'..'z'] [1..]
+    --     f (w, v) w_ = if v < v_ then (w_, v_) else (w, v)
+    --         where
+    --             v_ = sum $ map (fromJust . flip lookup table) w_
+
+    where
+        f (w, v) w_ = if v < v_ then (w_, v_) else (w, v)
+            where
+                v_ = sum $ map ord w_
