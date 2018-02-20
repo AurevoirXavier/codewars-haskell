d7d9cee5bb73620b3e0d3fb84466008c32d3d12a
Aurevoir Xavier <c.estlavie@icloud.com>
2018-02-21 01:49:09 +0800

    01:49/02/21/2018

--- 7kyu/Alphabet symmetry/lib.hs
+++ 7kyu/Alphabet symmetry/lib.hs
@@ -1,4 +1,8 @@
 module AlphabetSymm where
 
+import           Data.Char (ord, toLower)
+
 solve :: [String] -> [Int]
-solve xs = undefined
+solve = map (foldl (\cnt (i, c) -> if i == ord (toLower c) - 96 then cnt + 1 else cnt) 0 . zip [1..])
+
+-- solve = map $ length . filter id . zipWith (==) ['a'..'z'] . map toLower
