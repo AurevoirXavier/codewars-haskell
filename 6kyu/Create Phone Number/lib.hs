7decc6c2904d46b27a05ae04930754d39afdb5af
Aurevoir Xavier <c.estlavie@icloud.com>
2018-02-21 22:21:21 +0800

    22:21/02/21/2018

--- 6kyu/Create Phone Number/lib.hs
+++ 6kyu/Create Phone Number/lib.hs
@@ -1,3 +1,11 @@
 module CreatePhoneNumber where
 
+-- import           Text.Printf (printf)
+
 createPhoneNumber :: [Int] -> String
+createPhoneNumber xs = "(" ++ a ++ ") " ++ b ++ "-" ++ c
+    where
+        (a, tmp) = splitAt 3 (concatMap show xs)
+        (b, c)   = splitAt 3 tmp
+
+-- createPhoneNumber [a,b,c,d,e,f,g,h,i,j] = printf "(%d%d%d) %d%d%d-%d%d%d%d" a b c d e f g h i j
