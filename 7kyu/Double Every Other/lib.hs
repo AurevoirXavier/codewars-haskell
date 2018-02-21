0256e981dac36b786230b7b6ed8dfe5bf15728e1
Aurevoir Xavier <c.estlavie@icloud.com>
2018-02-21 21:03:40 +0800

    21:03/02/21/2018

--- 7kyu/Double Every Other/lib.hs
+++ 7kyu/Double Every Other/lib.hs
@@ -1,6 +1,4 @@
 module Codewars.Kata.DoubleEveryOther where
 
 doubleEveryOther :: [Integer] -> [Integer]
--- doubleEveryOther xs = fst $ foldr (\x (xs, flag) -> if flag then (x:xs, False) else ((2 * x):xs, True)) ([], odd (length xs)) xs
-
 doubleEveryOther = zipWith (*) (cycle [1,2])
