9ee943a33d14fffcdf971e9fb6740495190ada9a
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-01 16:13:53 +0800

    16:13/03/01/18

--- 6kyu/Simple arithmetic progression/lib.hs
+++ 6kyu/Simple arithmetic progression/lib.hs
@@ -1,6 +1,9 @@
 module SimpleArithProg where
 
+import           Data.Bits (shift)
+
 solve :: [Int] -> Int
-solve (x1:x2:xs) = if xs `elem` diff then 
+solve xs = if length xs > 2 then sum . map go . takeWhile ((/= 2) . length) $ iterate tail xs else 0
     where
-        x3 = 2 * n - p
+        go [_, _]     = 0
+        go (x1:x2:xs) = if (shift x2 1 - x1) `elem` xs then go (x1:xs) + 1 else go (x1:xs)
