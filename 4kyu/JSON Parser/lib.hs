25e591f91857cd5fa80325cb4e8c24e83be2a0d1
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-04 19:59:56 +0800

    19:59/03/04/18

---
+++ 4kyu/JSON Parser/lib.hs
@@ -0,0 +1,14 @@
+module JSON.Parser (parse) where
+
+-- import           JSON.Parser.Preloaded (Value (..))
+
+data Value = String String
+           | Number Double
+           | Object [(Value,Value)] -- an association list -- only a `String` is valid as the index `Value`
+           | Array [Value]          -- not limited to identical primitive datatypes
+           | Boolean Bool           -- either `True` or `False`
+           | Null
+
+parse :: String -> Maybe Value
+parse [] = Nothing
+parse 
