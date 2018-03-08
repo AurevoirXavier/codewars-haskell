d9f4b0af45e72a6803fca28543f448a873005185
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-08 20:27:42 +0800

    20:27/03/08/18

--- 7kyu/Credit card issuer checking/lib.hs
+++ 7kyu/Credit card issuer checking/lib.hs
@@ -1,5 +1,15 @@
 module Haskell.Codewars.CreditCardIssuer where
-    
+
 getIssuer :: Int -> String
 getIssuer number
-  -- Code your solution here
+    | (len == 13 || len == 14) && head n == '4'                   = "VISA"
+    | len == 15 && ((||) <$> (== "34") <*> (== "37") $ take 2 n)  = "AMEX"
+    | len == 16 =
+        if      take 4 n == "6011"                             then "Discover"
+        else if head n == '4'                                  then "VISA"
+        else if take 2 n `elem` ["51", "52", "53", "54", "55"] then "Mastercard"
+        else                                                        "Unknown"
+    | otherwise                                                   = "Unknown"
+    where
+        n   = show number
+        len = length n
