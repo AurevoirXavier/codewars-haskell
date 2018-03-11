7217e9e06fd418f086abb57669d7b7541573a486
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-11 12:47:33 +0800

    12:47/03/11/18

--- 6kyu/Simple nearest prime/lib.hs
+++ 6kyu/Simple nearest prime/lib.hs
@@ -1,7 +1,14 @@
 module SimpleNearestPrime where
 
+import           Data.List  (find)
+import           Data.Maybe (fromJust)
+
 solve :: Integer -> Integer
-solve n = x
+solve n = fromJust . find isPrime . filter (> 0) . concat $ zipWith (\a b -> [a, b]) (iterate (flip (-) 1) n) [n..]
     where
-        r = n `mod` 6
-        x = if r == 0 then n else n + 6 - r
+         isPrime n
+             | n == 2 || n == 3                 = True
+             | n `mod` 6 == 1 || n `mod` 6 == 5 = not $ any ((||) <$> (==) 0 . mod n <*> (==) 0 . mod n . (+2)) [5, 11 .. limit]
+             | otherwise                        = False
+             where
+                 limit = floor (sqrt (fromIntegral n))
