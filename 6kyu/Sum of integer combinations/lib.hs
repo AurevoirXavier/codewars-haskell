686728d95678b13ba8184329c46e58d82bdf238c
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-14 12:19:48 +0800

    12:19/03/14/18

--- 6kyu/Sum of integer combinations/lib.hs
+++ 6kyu/Sum of integer combinations/lib.hs
@@ -1,4 +1,8 @@
 module SumIntegerCombinations where
 
 find :: [Int] -> Int -> Int
-··
+find = ((length .) .) . go <$> id <*> length
+    where
+        go _  _   0   = [[]]
+        go _  0   _   = []
+        go xs len dif = [x:re | x <- xs, re <- go (filter (>= x) xs) (len - 1) (dif - x)]
