4b8a7bb3a033cb9abe198d166d9a8d667b94da4d
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-23 19:18:09 +0800

    19:18/03/23/2018

--- 7kyu/Sorted? yes? no? how?/lib.hs
+++ 7kyu/Sorted? yes? no? how?/lib.hs
@@ -1,4 +1,7 @@
 module Kata where
 
 isSortedAndHow :: [Int] -> String
-isSortedAndHow lst = 
+isSortedAndHow (x:xs)
+    | x < (last xs) = if any (<x) xs then "no" else "yes, ascending"
+    | x > (last xs) = if any (>x) xs then "no" else "yes, descending"
+    | otherwise     = if any (/=x) xs then "no" else "yes, ascending"
