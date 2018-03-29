279cce9e034437caaae7b3c6e051e110352e0f77
Aurevoir Xavier <c.estlavie@icloud.com>
2018-03-29 09:44:06 +0800

    09:44/03/29/18

--- 8kyu/Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence/lib.hs
+++ 8kyu/Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence/lib.hs
@@ -2,3 +2,4 @@ module Kata (replace) where
 
 replace :: String -> String
 replace = foldr (\x acc -> if x `elem` "aeiouAEIOU" then '!':acc else x:acc) ""
+-- replace = map (\x -> if x `elem` "aeiouAEIOU" then '!' else x)
