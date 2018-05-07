5268f1ae39e2b3b64d54f858c483bee68c85c14f
Xavier Lau <c.estlavie@icloud.com>
2018-05-07 20:25:23 +0800

    20:25/05/07/2018

--- 6kyu/Format words into a sentence/lib.hs
+++ 6kyu/Format words into a sentence/lib.hs
@@ -3,7 +3,8 @@ module FormatSentence where
 import           Data.List (intersperse)
 
 formatWords :: [String] -> String
-formatWords xs = case filter (not . null) xs of []     -> ""
-                                                [x]    -> x
-                                                [x, y] -> x ++ " and " ++ y
-                                                xs     -> (concat $ intersperse ", " (init xs)) ++ " and " ++ last xs
+formatWords xs = case filter (not . null) xs of
+    []     -> ""
+    [x]    -> x
+    [x, y] -> x ++ " and " ++ y
+    xs     -> (concat $ intersperse ", " (init xs)) ++ " and " ++ last xs
