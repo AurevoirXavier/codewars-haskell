65c5b1e339f7fd83fd7623d2374dd9e002487522
Xavier Lau <c.estlavie@icloud.com>
2018-05-17 12:25:51 +0800

    12:25/05/17/2018

---
+++ 6kyu/Do you speak retsec?/lib.hs
@@ -0,0 +1,10 @@
+module Codewars.Kata.Retsec where
+
+reverseByCenter :: String -> String
+reverseByCenter "" = ""
+reverseByCenter xs
+    | odd len   = splitAt (l 
+    | otherwise = 
+        where
+            len           = length xs
+            (left, right) = splitAt
